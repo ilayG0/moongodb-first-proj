7a5b7aafad2ecb4b81154fe2068c6e0d4ab02a9f
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2025-12-29 13:21:09 +0200

    change in commaon - npm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,12 +14,12 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host: "my-ec2-dev", 
-      ref: "origin/main", 
+      host: "my-ec2-dev",
+      ref: "origin/main",
       repo: "git@github.com:ilayG0/moongodb-first-proj.git",
       path: "/home/ubuntu/pokedex-server",
       "post-deploy":
-        "npm install && pm2 startOrReload ecosystem.config.js --env production",
+        'export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; npm install; pm2 startOrReload ecosystem.config.js --env production',
     },
   },
 };
