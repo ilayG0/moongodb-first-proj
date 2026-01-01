32fc676bc4284252d88fa828d3ba20d5d87d3eb3
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2026-01-01 10:33:14 +0200

    Add PM2 ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,28 +2,13 @@ module.exports = {
   apps: [
     {
       name: "pokedex-server",
-      script: "src/server.js", 
+      script: "npm",
+      args: "run dev",
+      instances: 1,
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
       },
     },
   ],
-
-  deploy: {
-    production: {
-      user: "ubuntu",
-      host: "my-ec2-dev",
-      ref: "origin/main",
-      repo: "git@github.com:ilayG0/moongodb-first-proj.git",
-      path: "/home/ubuntu/pokedex-server",
-      ssh_options: "StrictHostKeyChecking=no",
-      "pre-deploy-local": "echo 'Deploying code to EC2'",
-      "post-deploy":
-        `. /home/ubuntu/.nvm/nvm.sh; npm install; pm2 startOrReload ecosystem.config.js --env production`,
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-  },
 };
